bc0c5b7ae9eb5c3a6a2802d0c2ba4c31b7a5f1d2
ZeeDabs <dabbagh.zainab@gmail.com>
2023-02-20 20:30:35 -0500

    deleted empty lines at the end of the code

--- script.js
+++ script.js
@@ -88,39 +88,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
