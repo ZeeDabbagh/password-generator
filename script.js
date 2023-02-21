2828ce56cda7ad2dae20bb310845258da1a1ea65
ZeeDabs <dabbagh.zainab@gmail.com>
2023-02-20 19:51:25 -0500

    created an empty readme file, along with final code with typed out arrays for upper, lower, number and special characters

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
-// second try
-// ____________________
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// // Learned .fromCharCode from day 1 of the "Third Party API" module
 var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// String.fromCharCode(41,90)
 var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-// String.fromCharCode(141,172)
 var numberChar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-// String.fromCharCode(48,57)
 var specialChar = ["!", "@", "#", "$", "&", "*", "(", ")", "_", "-", ">", "<", "?"];
-// String.fromCharCode(33,47);
-
-
-
 
 function generatePassword() {
   var password = [];
-  var passLength = parseInt(prompt("Please enter your desired length"))
+  var passLength = prompt("Please enter your desired length")
   // After asking a tutor how to reject strings from passLength input, he taught me isNaN
   if (passLength < 8 || passLength > 128 || isNaN(passLength) || passLength === "") {
     alert("Please try again. Enter a value that meets the criteria(length between 8-128)");
@@ -44,7 +33,9 @@ function generatePassword() {
   if (includesSpecialChar) {
     password = password.concat(specialChar)
   }
-  password = password.join("")
+
+  password = password.join("");
+
   var newPass = "";
   var count = 0
   if (includesUpperCase) {
@@ -71,6 +62,7 @@ function generatePassword() {
       var randomIndex = Math.floor(Math.random() * password.length );
       newPass = newPass + password[randomIndex];
     }
+    // To randomize password output, from Stack Overflow
     newPass = newPass.split('').sort(function(){return 0.5-Math.random()}).join('');
 
   return newPass;
@@ -87,8 +79,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// ---------------------------------------------------------
 
 
 
